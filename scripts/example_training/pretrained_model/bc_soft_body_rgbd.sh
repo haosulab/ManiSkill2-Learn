f98e89116ad90ddba3317ca299c365f216e8db18
xuanlinli17 <xuanlinli17@gmail.com>
2022-10-20 21:03:09 -0700

    modify scripts/example_training such that code can be directly run w/o modifying

--- scripts/example_training/pretrained_model/bc_soft_body_rgbd.sh
+++ scripts/example_training/pretrained_model/bc_soft_body_rgbd.sh
@@ -1,7 +1,9 @@
+# Replace --work-dir, env_cfg.env_name, and replay_cfg.buffer_filenames when you run other environments
+
 python maniskill2_learn/apis/run_rl.py configs/brl/bc/rgbd_soft_body.py \
---work-dir YOUR_LOGGING_DIRECTORY --gpu-ids 0 \
---cfg-options "env_cfg.env_name=Fill-v0" "env_cfg.obs_mode=rgbd" "env_cfg.n_points=1200" \
+--work-dir ./logs/dapg_excavate_rgbd --gpu-ids 0 \
+--cfg-options "env_cfg.env_name=Excavate-v0" "env_cfg.obs_mode=rgbd" "env_cfg.n_points=1200" \
 "env_cfg.control_mode=pd_joint_delta_pos" \
-"replay_cfg.buffer_filenames=PATH_TO_RGBD_DEMO" \
+"replay_cfg.buffer_filenames=../ManiSkill2/demos/rigid_body_envs/Excavate-v0/trajectory.none.pd_ee_delta_pose_rgbd.h5" \
 "eval_cfg.num=100" "eval_cfg.save_traj=False" "eval_cfg.save_video=True" \
 "train_cfg.n_eval=50000" "train_cfg.total_steps=50000" "train_cfg.n_checkpoint=50000" "train_cfg.n_updates=500"
--- scripts/example_training/pretrained_model/dapg_stackcube_pointcloud.sh
+++ scripts/example_training/pretrained_model/dapg_stackcube_pointcloud.sh
@@ -3,10 +3,10 @@
 # if you only have one GPU with small memory, then you can set a smaller rollout_cfg.num_procs (e.g. =5)
 
 python maniskill2_learn/apis/run_rl.py configs/mfrl/dapg/maniskill2_pn.py \
-            --work-dir YOUR_LOGGING_DIRECTORY --gpu-ids 0 --sim-gpu-ids 1 \
+            --work-dir ./logs/dapg_stackcube_pointcloud --gpu-ids 0 --sim-gpu-ids 1 \
             --cfg-options "env_cfg.env_name=StackCube-v0" "env_cfg.obs_mode=pointcloud" "env_cfg.n_points=1200" \
             "rollout_cfg.num_procs=16" "env_cfg.reward_mode=dense" \
             "env_cfg.control_mode=pd_ee_delta_pose" "env_cfg.obs_frame=ee" \
-            "agent_cfg.demo_replay_cfg.buffer_filenames=PATH_TO_POINT_CLOUD_DEMO.h5" \
+            "agent_cfg.demo_replay_cfg.buffer_filenames=../ManiSkill2/demos/rigid_body_envs/StackCube-v0/trajectory.none.pd_ee_delta_pose_pointcloud.h5" \
             "eval_cfg.num=100" "eval_cfg.save_traj=False" "eval_cfg.save_video=True" \
             "train_cfg.total_steps=25000000" "train_cfg.n_checkpoint=5000000"
