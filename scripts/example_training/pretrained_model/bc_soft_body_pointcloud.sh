a7d7ba5d71e26ddaa0f6445abe503d30f4e86fe6
xuanlinli17 <xuanlinli17@gmail.com>
2023-01-24 13:12:42 -0800

    change scripts/example_training  demo loading path from rigid/soft_body_envs to rigid/soft_body to factor in recent changes in ManiSkill2 0.4.0

--- scripts/example_training/pretrained_model/bc_soft_body_pointcloud.sh
+++ scripts/example_training/pretrained_model/bc_soft_body_pointcloud.sh
@@ -4,6 +4,6 @@ python maniskill2_learn/apis/run_rl.py configs/brl/bc/pointnet_soft_body.py \
 --work-dir ./logs/dapg_excavate_pointcloud --gpu-ids 0 \
 --cfg-options "env_cfg.env_name=Excavate-v0" "env_cfg.obs_mode=pointcloud" "env_cfg.n_points=1200" \
 "env_cfg.control_mode=pd_joint_delta_pos" \
-"replay_cfg.buffer_filenames=../ManiSkill2/demos/rigid_body_envs/Excavate-v0/trajectory.none.pd_ee_delta_pose_pointcloud.h5" \
+"replay_cfg.buffer_filenames=../ManiSkill2/demos/rigid_body/Excavate-v0/trajectory.none.pd_ee_delta_pose_pointcloud.h5" \
 "eval_cfg.num=100" "eval_cfg.save_traj=False" "eval_cfg.save_video=True" \
 "train_cfg.n_eval=50000" "train_cfg.total_steps=50000" "train_cfg.n_checkpoint=50000" "train_cfg.n_updates=500"
