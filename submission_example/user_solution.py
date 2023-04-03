81487b2029891dd596d85994f54313aa1ead35cf
xuanlinli17 <xuanlinli17@gmail.com>
2023-04-03 16:10:01 -0700

    more fixes on dockerfile and submission example

--- submission_example/user_solution.py
+++ submission_example/user_solution.py
@@ -1,4 +1,4 @@
-# Put this file directly under ManiSkill2-Learn/
+# REMEMBER: Move this file directly under ManiSkill2-Learn/
 
 import numpy as np
 import torch
@@ -18,20 +18,28 @@ from maniskill2_learn.utils.data import GDict, is_not_null
 class UserPolicy(BasePolicy):
     def __init__(self, env_id: str, observation_space: spaces.Space, action_space: spaces.Space):
         super().__init__(env_id, observation_space, action_space)
-        cfg = Config.fromfile("/root/ManiSkill2-Learn/configs/mfrl/ppo/maniskill2_pn.py")
-        model_path = '/root/ManiSkill2-Learn/work_dir/pn/models/model_2000.ckpt'
+        cfg = Config.fromfile("/root/ManiSkill2-Learn/configs/mfrl/ppo/maniskill2_pn.py") # Change this
+        model_path = f'/root/ManiSkill2-Learn/maniskill2_learn_pretrained_models_videos/{env_id}/dapg_pointcloud/model_25000000.ckpt' # Change this
         self.device = 'cuda:0'
 
         self.logger = get_logger()
 
         cfg.env_cfg["env_name"] = env_id
+        cfg.env_cfg["obs_mode"] = self.get_obs_mode(env_id)
+        cfg.env_cfg["control_mode"] = self.get_control_mode(env_id)
+        cfg.env_cfg["obs_frame"] = "ee" # Change this
+        if 'Pick' in env_id: # for environments that provide goal locations
+            cfg.env_cfg["n_goal_points"] = 50 # Change this
+            
         env_params = get_env_info(cfg.env_cfg)
         cfg.agent_cfg["env_params"] = env_params
         obs_shape = env_params["obs_shape"]
         action_shape = env_params["action_shape"]
+        
         if is_not_null(obs_shape) or is_not_null(action_shape):
             replaceable_kwargs = get_kwargs_from_shape(obs_shape, action_shape)
             cfg = replace_placeholder_with_args(cfg, **replaceable_kwargs)  
+            
         print("Final config", cfg)
 
         self.env = build_env(cfg.env_cfg)
@@ -53,8 +61,10 @@ class UserPolicy(BasePolicy):
 
     @classmethod
     def get_obs_mode(cls, env_id: str) -> str:
+        # Change this
         return "pointcloud"
 
     @classmethod
     def get_control_mode(cls, env_id: str) -> str:
-        return "pd_joint_delta_pos"
+        # Change this
+        return "pd_ee_delta_pose"
